e5bb9b62e6c700822e362f3e8e9dbe0d551b5d24
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-04-02 18:00:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,20 @@ int main(){
     float a,b;
     int opzione;
 
-    cout<<"inserisci il valore a: "<<endl;
-    cin>> a;
-    cout<<"inserisci il valore di b: "<<endl;
+    cin>>a;
     cin>>b;
-    cout<<"inserire il numero corrispondente alla forma geometrica di cui calcolare l'area ( 1 Triangolo, 2 Quadrato, 3 Rettangolo): "<<endl;
     cin>>opzione;
-
-    if(!cin or opzione>2){
-        cout<<"Errore"<<endl;
-        return -1;
-    }
-
+    
     switch(opzione){
-        case 1:
-            cout<< ((a*b)/2)<<endl;
+        case 0:
+            cout<<((a*b)/2)<<endl;
             break;
 
-        case 2:
+        case 1:
             cout<<(a*a)<<endl;
             break;
 
-        case 3:
+        case 2:
             cout<<(a*b)<<endl;
             break;
 
